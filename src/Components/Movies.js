1dfd58a84089ea1053c092bfb31d9d72a68db9a1
Stefan <120310018+stefanlatic@users.noreply.github.com>
2025-01-26 13:47:47 +0100

    Searching Movies From OMDb API with src, title, description

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,22 +1,27 @@
 import axios from "axios";
-import { useState } from 'react';
-
+import {  useState } from 'react';
+import MovieCard from "../Templates/Snippets/MovieCard.jsx";
 
 const Movies = () => {
 
-    let[movieName, setMovieName] = useState("");
+    let[movieName, setMovieName] = useState("Lord of the Rings");
+    let[movieData, setMovieData] = useState({});
 
     const searchMovieByTitle = () => {
         axios.get(process.env.REACT_APP_OMDBAPI_URL+"?t="+movieName+"&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
-      .then(response => console.log(response.data))
+      .then(response => setMovieData(response.data))
       .catch(error => console.log(error))
     }
+    searchMovieByTitle();
+    
     return(
         <>
-
+    <form onSubmit={ e => e.preventDefault() }>
         <input type="text" onInput={(e) => setMovieName(e.currentTarget.value)}></input>
         <button onClick={searchMovieByTitle}>Search Movie</button>
+    </form>
 
+        <MovieCard movie= {movieData} />
         </>
     )
 } 
